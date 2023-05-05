37bdbb8b9eba008c39d61b2cc928ad74c5848fec
AgnoDev <agnodev87@gmail.com>
2023-05-05 15:14:51 -0300

    cria interface ParkingSpotController e injeta Service

---
+++ src/main/java/com/api/estacionamento/controllers/ParkingSpotController.java
@@ -0,0 +1,19 @@
+package com.api.estacionamento.controllers;
+
+import com.api.estacionamento.services.ParkingSpotService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/estacionamento")
+public class ParkingSpotController {
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+
+}
